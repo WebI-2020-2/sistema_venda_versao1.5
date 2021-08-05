9b8db80a41d28f48baf30dcb9ecd7ac6e033a3b9
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2021-08-05 15:50:30 -0300

    Delete views directory

--- views/Realizas_Vendas/ItensVenda/forma_pagamento/forma_pagamento/CrudFormaPagamento/conexao/Config_FormaPagamento.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define('HOST_FormaPagamento','localhost');
-	define('USER_FormaPagamento','postgres');
-	define('PASSWORD_FormaPagamento','123456');
-	define('BASE_FormaPagamento','venda');
-
-?>
